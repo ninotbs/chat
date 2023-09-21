c0df0ac71432db0d3aef5da10c29bcddeb1809aa
ninotbs <nino_tbs@mac.com>
2023-09-21 21:31:51 +0200

    server socket successfully created

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const socket = require('socket.io')
+const authRouter = require('./routes/auth')
+const messageRoutes = require('./routes/messages')
 
 const app = express()
 require('dotenv').config()
@@ -17,6 +20,31 @@ mongoose.connect(process.env.MONGO_URL, {
   console.log(err.message)
 })
 
+app.use('/api/auth', authRouter)
+app.use('/api/messages', messageRoutes)
+
 const server = app.listen(process.env.PORT, () => {
   console.log(`Server is running on port: ${process.env.PORT}`)
+})
+
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true,
+  },
+})
+
+global.onlineUsers = new Map()
+io.on('connection', (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id)
+  });
+
+  socket.on('send-msg', (data) => {
+    const sendUserSocket = onlineUsers.get(data.to)
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.msg)
+    }
+  })
 })
