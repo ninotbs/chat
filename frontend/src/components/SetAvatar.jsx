490454f35b3c20693894cfa9daab796d86f3e652
ninotbs <nino_tbs@mac.com>
2023-09-21 21:14:44 +0200

    components completed successfully

--- frontend/src/components/SetAvatar.jsx
+++ frontend/src/components/SetAvatar.jsx
@@ -1,15 +1,69 @@
 import React from 'react'
 import styled from 'styled-components'
 import loader from '../assets/loader.gif'
-import { ToastContainer } from 'react-toastify'
+import { ToastContainer, toast } from 'react-toastify'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import { Buffer } from 'buffer'
+import { setAvatarRoute } from '../utils/APIRoutes'
 
 function SetAvatar() {
+  const api = `https://api.multiavatar.com/Starcrasher.png?apikey=vURqdLPrFaXn1O`
+  const navigate = useNavigate()
+  const [avatars, setAvatars] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [selectedAvatar, setSelectedAvatar] = React.useState(undefined);
+  const toastOptions = {
+    position: 'bottom-right',
+    autoClose: 8000,
+    pauseOnHover: true,
+    draggable: true,
+    theme: 'dark'
+  }
+  
+  React.useEffect(async () => {
+    if (!localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY))
+      navigate('/login')
+  }, [])
 
-  const [isLoading] = React.useState(false)
-  const [setSelectedAvatar] = React.useState(undefined)
-  const [avatars] = React.useState([])
-  const [selectedAvatar] = React.useState(undefined)
-  const [setProfilePicture] = React.useState(undefined)
+  const setProfilePicture = async () => {
+    if (selectedAvatar === undefined) {
+      toast.error('Please select an avatar', toastOptions)
+    } else {
+      const user = await JSON.parse(
+        localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)
+      )
+
+      const { data } = await axios.post(`${setAvatarRoute}/${user._id}`, {
+        image: avatars[selectedAvatar],
+      })
+
+      if (data.isSet) {
+        user.isAvatarImageSet = true
+        user.avatarImage = data.image
+        localStorage.setItem(
+          process.env.REACT_APP_LOCALHOST_KEY,
+          JSON.stringify(user)
+        );
+        navigate('/')
+      } else {
+        toast.error('Error setting avatar. Please try again.', toastOptions)
+      }
+    }
+  }
+
+  React.useEffect(async () => {
+    const data = []
+    for (let i = 0; i < 4; i++) {
+      const image = await axios.get(
+        `${api}/${Math.round(Math.random() * 1000)}`
+      )
+      const buffer = new Buffer(image.data)
+      data.push(buffer.toString("base64"))
+    }
+    setAvatars(data)
+    setIsLoading(false)
+  }, [])
 
   return (
     <>
